da53593723bcd453f6d528fc5cc431062e90af25
Sh3tss <gabrielggartist@gmail.com>
2025-06-02 18:45:56 -0300

    change the js and css code to see if this hellp

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const Container = document.getElementById('container');
+const container = document.getElementById('container');
 const gridSize = 16;
 
-function createGrid(){
-    for (let i = 0; i < gridSize; i++){
-        const row = document.createElement('div');
-        row.classList.add('grid-row');
 
-        for (let j = 0; j < gridSize; j++){
-            const cell = document.createElement('div');
-            cell.classList.add('grid-cell');
-            row.appendChild(cell);
-        }
-        Container.appendChild(row);
+for (let i = 0; i < gridSize; i++){
+    for (let j = 0; j < gridSize; j++){
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    container.appendChild(square);
     }
 }
