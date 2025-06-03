575ba5f7fd7d214310b8e34305d0667fb508bcb7
Sh3tss <gabrielggartist@gmail.com>
2025-06-03 18:27:57 -0300

    a new code with many changes to put everything working

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const container = document.getElementById('container');
-const gridSize = 16;
-
-
-for (let i = 0; i < gridSize; i++){
-    for (let j = 0; j < gridSize; j++){
-    const square = document.createElement('div');
-    square.classList.add('grid-square');
-    container.appendChild(square);
+function makeGrids(size){
+    let screen = document.querySelector(".sketch-screen");
+    for (let i = 0; i < size; i++){
+        let column  = document.createElement("div");
+        column.classList.add("column");
+        for (let j = 1; j <= size; j++){
+            let row = document.createElement("div");
+            row.classList.add("row");
+            row.style.border = "2px solid black";
+            row.innerText = (i * size) + j;
+            column.appendChild(row);
+        }
+        screen.appendChild(column);
     }
 }
+makeGrids(16); 
