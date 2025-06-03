8053b319ce86ca0e7d6cf56ff95895c1b324d85b
Sh3tss <gabrielggartist@gmail.com>
2025-06-03 19:20:26 -0300

    the grid showed up now its time to refine it

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeGrids(size){
     let container = document.querySelector(".container");
+    
+    console.log(container);
+
+    if (!container) {
+        console.error("Erro: O elemento .container não foi encontrado no HTML.");
+        return; // Sai da função se o container não for encontrado
+    }
+
+    container.innerHTML = '';
     for (let i = 0; i < size; i++){
         let gridRow  = document.createElement("div");
         gridRow.classList.add("grid-row");
@@ -11,4 +20,7 @@ function makeGrids(size){
         container.appendChild(gridRow);
     }
 }
-makeGrids(16); 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM totalmente carregado. chamando makegrids(16)");
+    makeGrids(16);
+});
